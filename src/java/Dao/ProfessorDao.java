798d01cb515c10888057dd919e893646bd5450ed
ASKJR <albertokatojr@gmail.com>
2016-09-04 21:46:58 -0300

    Finalizando o exemplo de ProfessorDao

--- src/java/Dao/ProfessorDao.java
+++ src/java/Dao/ProfessorDao.java
@@ -8,7 +8,10 @@ package Dao;
 import Beans.Professor;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -17,9 +20,13 @@ import java.sql.SQLException;
 public class ProfessorDao {
     /*SQL*/
     private final static String INSERT = "INSERT INTO professor (nome) VALUES (?)";
+    private final static String DELETE = "DELETE FROM professor WHERE id_prof=?";
+    private final static String UPDATE = "UPDATE professor SET nome=? WHERE id_prof=?";
+    private final static String SELECT = "SELECT * FROM professor";
     
     /*DB variables*/
     private Connection con         = null;
+    private ResultSet rs           = null;
     private PreparedStatement stmt = null;
 
     public ProfessorDao() {
@@ -37,4 +44,55 @@ public class ProfessorDao {
             try { if (con  != null) con.close();  } catch (Exception e) {};
         }
     }
+    public void update(Professor professor){
+        try{
+                con  = new ConnectionFactory().getConnection();
+                stmt = con.prepareStatement(UPDATE);	
+                stmt.setString(1,professor.getNome());
+                stmt.setLong(2,professor.getId());
+                stmt.execute();
+        }catch(SQLException e){
+                 throw new RuntimeException(e);
+        }finally{
+                 try { if (stmt != null) stmt.close(); } catch (Exception e) {};
+                 try { if (con  != null) con.close();  } catch (Exception e) {};
+        }
+    }
+    public List<Professor> getAllContacts() {
+        try {
+            con  = new ConnectionFactory().getConnection();
+            stmt = con.prepareStatement(SELECT);
+            List<Professor> professores = new ArrayList<>();
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Professor professor = new Professor();
+                professor.setId(rs.getInt("id_prof"));
+                professor.setNome(rs.getString("nome"));
+                professores.add(professor);
+            }
+            return professores;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally{
+            try { if (rs   != null) stmt.close();   } catch (Exception e) {};
+            try { if (stmt != null) stmt.close();   } catch (Exception e) {};
+            try { if (con  != null) con.close();    } catch (Exception e) {};
+        }
+    }
+    public void delete(Professor professor) {
+        try {
+            con = new ConnectionFactory().getConnection();
+            stmt = con.prepareStatement(DELETE);
+            stmt.setLong(1, professor.getId());
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally{
+            try { if (stmt != null) stmt.close(); } catch (Exception e) {};
+            try { if (con  != null) con.close();  } catch (Exception e) {};
+        }
+    }
+    
 }
