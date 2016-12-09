2127c9d1f382afc44b90ab1b238ee70a65266722
ASKJR <albertokatojr@gmail.com>
2016-12-08 23:33:42 -0200

    BUGFIX: Correção Dashboard Pizza -- Erro operador Lógico.
    FALTA ADICIONAR O ID_PROFESSOR....

--- src/java/Dao/DashboardDao.java
+++ src/java/Dao/DashboardDao.java
@@ -38,11 +38,11 @@ public class DashboardDao {
     "SELECT count(*) as emAndamento FROM avaliacao Where submissao_inicial < now() submissao_final > now()";
  
     private final static String GRAFICO_FASES_AVALIACAO = 
-    "SELECT"
-    +"(SELECT COUNT(*) AS naoIniciadas FROM avaliacao WHERE submissao_inicial > NOW()) AS NaoIniciadas,"
-    +"(SELECT COUNT(*) AS emSubmissao FROM avaliacao WHERE submissao_inicial >= NOW() AND submissao_final <= NOW()) AS emSubmissao,"
-    +"(SELECT COUNT(*) AS emCorrecao FROM avaliacao WHERE correcao_inicial >= NOW() AND correcao_final <= NOW()) AS emCorrecao,"
-    +"(SELECT COUNT(*) AS finalizadas FROM avaliacao WHERE NOW() > correcao_final) AS finalizadas";
+    "SELECT "
+   +"(SELECT COUNT(*) AS naoIniciadas FROM avaliacao WHERE submissao_inicial > NOW()) AS NaoIniciadas, "
+   +"(SELECT COUNT(*) AS emSubmissao FROM avaliacao WHERE NOW() >= submissao_inicial AND NOW() <= submissao_final) AS emSubmissao, "
+   +"(SELECT COUNT(*) AS emCorrecao FROM avaliacao WHERE NOW() >= correcao_inicial AND NOW() <= correcao_final ) AS emCorrecao, "
+   +"(SELECT COUNT(*) AS finalizadas FROM avaliacao WHERE NOW() > correcao_final) AS finalizadas ";
             
     private final static String TOP_CORRETORES = 
     "SELECT  usAluno.nome, count(*) AS qtdCorrecoes FROM coavalieitor_db.correcao cor "
